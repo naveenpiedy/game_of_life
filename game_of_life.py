35754d85de09b0fc1f526b86d4b4157725a084e6
Naveen Piedy <nav.bugs.thepoet@gmail.com>
2020-08-03 22:40:39 -0400

    ironed out bugs

--- game_of_life.py
+++ game_of_life.py
@@ -1,57 +1,57 @@
-import itertools
-
+import random
 import pygame
 import numpy as np
 
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
 HEIGHT = 300
 WIDTH = 300
-
-
-def get_neighbour(screen, x, y):
-    x_values = [x - 1, x, x + 1]
-    y_values = [y - 1, y, y + 1]
-    neighbouring_points = list(itertools.product(x_values, y_values))
-    neighbouring_points = filter(
-        lambda item: not (item[0] >= HEIGHT or item[0] < 0 or item[1] >= WIDTH or item[1] < 0), neighbouring_points)
-
-    return neighbouring_points
 
 
 def game_of_life_rules(screen, x, y):
     dead = []
     live = []
-    neighbouring_points = get_neighbour(screen, x, y)
-    live_neighbours = filter(lambda item: screen.get_at(item) == WHITE, neighbouring_points)
-    num_live_neighbours = len(list(live_neighbours))
+    x_values = [x - 1, x, x + 1]
+    y_values = [y - 1, y, y + 1]
+    live_neighbours = 0
+    for i in x_values:
+        for j in y_values:
+            if 0 <= i < HEIGHT and 0 <= j < WIDTH and (i, j) != (x, y):
+                if screen.get_at((i, j)) == WHITE:
+                    live_neighbours += 1
 
-    if screen.get_at((x, y)) == WHITE:
-        if not (2 < num_live_neighbours < 4):
-            dead.append((x, y))
-    else:
-        if num_live_neighbours == 3:
-            live.append((x, y))
+    if screen.get_at((x, y)) == WHITE and not 1 < live_neighbours < 4:
+        dead.append((x, y))
+    elif screen.get_at((x, y)) == BLACK and live_neighbours == 3:
+        live.append((x, y))
 
     return live, dead
 
 
 def init_screen(screen):
-    pygame.draw.line(screen, WHITE, (0, 299), (299, 0), 10)
-    pygame.draw.line(screen, WHITE, (0, 0), (299, 299), 10)
+    i = screen
+    i[120][120] = WHITE
+    i[121][120] = WHITE
+    i[120][121] = WHITE
+    i[121][121] = WHITE
+
+    i[122][119] = WHITE
+    i[123][119] = WHITE
+    i[123][118] = WHITE
+    i[122][118] = WHITE
 
 
 def game_loop():
-    size = (HEIGHT, WIDTH)
-    screen = pygame.display.set_mode(size)
+    screen = pygame.Surface((WIDTH, HEIGHT))
+    win = pygame.display.set_mode((WIDTH*6, HEIGHT*6))
     pygame.display.set_caption("Game of Life")
     screen.fill(BLACK)
-    init_screen(screen)
+    pixAr = pygame.PixelArray(screen)
+    init_screen(pixAr)
+    win.blit(pygame.transform.scale(screen, win.get_rect().size), (0, 0))
+    pygame.display.update()
     carry_on = True
-    clock = pygame.time.Clock()
     while carry_on:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -65,18 +65,17 @@ def game_loop():
                 alive.extend(l)
             if d:
                 dead.extend(d)
-
         if alive:
             for i in alive:
-                pygame.draw.circle(screen, WHITE, i, 1, 1)
+                pixAr[i[0]][i[1]] = WHITE
 
         if dead:
             for i in dead:
-                pygame.draw.circle(screen, BLACK, i, 1, 1)
+                pixAr[i[0]][i[1]] = BLACK
 
-        pygame.display.flip()
 
-        clock.tick(120)
+        win.blit(pygame.transform.scale(screen, win.get_rect().size), (0, 0))
+        pygame.display.update()
 
     pygame.quit()
 
